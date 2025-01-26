d4cd2be6c7fbe9b61bf8eb99d836740be6fb3af2
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-26 23:54:41 +0530

    We can use recharts library to display the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
+
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 function App() {
   
   const data = [
     {name: "Facebook", value: 20000000},
     {name: "Instagram", value: 30000000},
-    {name: "Twitter", value: 15000000}
+    {name: "Twitter", value: 15000000},
+    {name: "Telegram", value: 1000000}
   ]
 
   return (
     <>
-      <h1>HELLO</h1>
+       <h1>Social Media Users Chart</h1>
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          
+          <Tooltip />
+        </PieChart>
+   
     </>
   )
 }
