a09ead0f72cfcabbc6cdd0861a1ddee985e19666
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-26 23:39:13 +0530

    Data displayer using charts

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  
+  const data = [
+    {name: "Facebook", value: 20000000},
+    {name: "Instagram", value: 30000000},
+    {name: "Twitter", value: 15000000}
+  ]
+
+  return (
+    <>
+      <h1>HELLO</h1>
+    </>
+  )
+}
+
+export default App
